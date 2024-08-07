68410f7d28b192e3acd652bb896e064539a83e3f
RaulCudris <raulcudriz@hotmail.com>
2024-08-07 16:38:56 -0500

    first commit

---
+++ src/main/java/factorymethod/MageFactory.java
@@ -0,0 +1,8 @@
+package factorymethod;
+
+public class MageFactory extends EnemyFactory {
+    @Override
+    public Enemy createEnemy() {
+        return new Mage();
+    }
+}
---
+++ src/main/java/factorymethod/WarriorFactory.java
@@ -0,0 +1,7 @@
+package factorymethod;
+public class WarriorFactory extends EnemyFactory {
+    @Override
+    public Enemy createEnemy() {
+        return new Warrior();
+    }
+}
