4efa89559bcf05aed99b9c2f3a8434800682aea1
RaulCudris <raulcudriz@hotmail.com>
2024-08-07 19:31:47 -0500

    implementacion del patron singleton

---
+++ src/main/java/singleton/TestSingleton.java
@@ -0,0 +1,29 @@
+package singleton;
+
+public class TestSingleton {
+    public static void main(String[] args) {
+        Thread threadRaulongo = new Thread( new RunRaulongo());
+        Thread threadJoshongo = new Thread( new RunJoshongo());
+
+        threadRaulongo.start();
+        threadJoshongo.start();
+    }
+
+    static class RunRaulongo implements Runnable{
+        @Override
+        public void run() {
+            Player player = Player.getPlayer("Raulongo");
+            System.out.println("Running Raulongo "+player);
+        }
+    }
+
+    static class RunJoshongo implements Runnable{
+
+        @Override
+        public void run() {
+            Player player = Player.getPlayer("Joshongo");
+            System.out.println("Running Joshongo "+player);
+        }
+    }
+
+}
